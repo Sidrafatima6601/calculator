7b0ec1d2f7bf3e4205675d07dbe71749b91c1690
Sidrafatima6601 <165972146+Sidrafatima6601@users.noreply.github.com>
2024-04-27 23:57:46 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+// asking questions from users through inquirer
+let answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstNumber" },
+    { message: "enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "select one operator to perform operations",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "subtractions", "multiplication", "division"],
+    },
+]);
+// conditional statements if-else
+if (answer.operator === "addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "subtractions") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else {
+    console.log("invalid input");
+}
